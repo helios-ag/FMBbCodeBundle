62419568f2d271805f520e64bcd6fa8bba4ae723
Al Ganiev <helios.ag@gmail.com>
2013-02-01 15:37:22 +1100

    fixed tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-define('DECODA', __DIR__.'/../../../../../mjohnson/decoda/src/Decoda');
-
 $file = __DIR__.'/../vendor/autoload.php';
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies to run test suite.');
