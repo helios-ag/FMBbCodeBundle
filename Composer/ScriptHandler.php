b670dfca6148a88860eb0b95dd7a61c27375699a
Al Ganiev <helios.ag@gmail.com>
2013-07-11 02:12:14 +1100

    automate emoticons dump

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -3,40 +3,21 @@
 namespace FM\BbcodeBundle\Composer;
 
 use Symfony\Component\Process\Process;
-use Symfony\Component\Process\PhpExecutableFinder;
+use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as BaseHandler;
 
 /**
- * @author Al Ganiev, original by Jordi Boggiano <j.boggiano@seld.be>
+ * @author Al Ganiev, original from LiipMonitorBundle by @pulse00
+ * Composer ScriptHandler can be used to run postInstall/postUpdate dump emoticons
+ * when running composer.phar update/install.
  */
 class ScriptHandler
 {
     public static function installEmoticons($event)
     {
-        static::executeCommand($event, 'app', 'bbcode:dump');
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+        $event->getIO()->write('<info>Dumping emoticons...</info>');
+        static::executeCommand($event, $appDir, 'bbcode:dump');
     }
 
-    protected static function getPhp()
-    {
-        $phpFinder = new PhpExecutableFinder;
-        if (!$phpPath = $phpFinder->find()) {
-            throw new \RuntimeException('The php executable could not be found, add it to your PATH environment variable and try again');
-        }
-
-        return $phpPath;
-    }
-
-    protected static function executeCommand($event, $appDir, $cmd, $timeout = 300)
-    {
-        $php = escapeshellarg(self::getPhp());
-        $console = escapeshellarg($appDir.'/console');
-        if ($event->getIO()->isDecorated()) {
-            $console.= ' --ansi';
-        }
-
-        $process = new Process($php.' '.$console.' '.$cmd, null, null, null, $timeout);
-        $process->run(function ($type, $buffer) { echo $buffer; });
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd)));
-        }
-    }
 }
