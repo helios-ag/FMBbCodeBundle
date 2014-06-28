b5b57c2c0031c76f6587cdab396146a885c650ee
Alexandre Quercia <alquerci@email.com>
2014-06-28 23:38:19 +0200

    Added default emoticons configuration loaded from Decoda

---
+++ Resources/config/emoticons.php
@@ -0,0 +1,26 @@
+<?php
+
+use Decoda\Decoda;
+use Decoda\Hook\EmoticonHook;
+
+use FM\BbcodeBundle\Emoticon\EmoticonCollection;
+use FM\BbcodeBundle\Emoticon\Emoticon;
+
+// Convert a default decoda emoticons array to an EmoticonCollection
+$collection = new EmoticonCollection();
+
+$decoda = new Decoda();
+$hook = new EmoticonHook();
+$hook->setParser($decoda);
+$hook->startup();
+$emoticons = $hook->getEmoticons();
+
+foreach ($emoticons as $name => $smilies) {
+    $emoticon = new Emoticon();
+    foreach ($smilies as $smiley) {
+        $emoticon->setSmiley($smiley);
+    }
+    $collection->add($name, $emoticon);
+}
+
+return  $collection;
