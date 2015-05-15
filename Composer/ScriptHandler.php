cc0725e6e20b14d834bafa3d57c12bc68eabc51e
Albert Ganiev <helios.ag@gmail.com>
2015-05-15 12:16:32 +1000

    fix composer script and readme ttypo

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -4,6 +4,8 @@ namespace FM\BbcodeBundle\Composer;
 
 use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as BaseHandler;
 
+use Composer\Script\CommandEvent;
+
 /**
  * @author Al Ganiev, original from LiipMonitorBundle by @pulse00
  * Composer ScriptHandler can be used to run postInstall/postUpdate dump emoticons
@@ -11,12 +13,13 @@ use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as BaseHandler;
  */
 class ScriptHandler extends BaseHandler
 {
-    public static function installEmoticons($event)
+    public static function installEmoticons(CommandEvent $event)
     {
         $options = self::getOptions($event);
-        $appDir = $options['symfony-app-dir'];
+        // use Symfony 3.0 dir structure if available
+        $consoleDir = isset($options['symfony-bin-dir']) ? $options['symfony-bin-dir'] : $options['symfony-app-dir'];
         $event->getIO()->write('<info>Dumping emoticons...</info>');
-        static::executeCommand($event, $appDir, 'bbcode:dump');
+        static::executeCommand($event, $consoleDir, 'bbcode:dump');
     }
 
 }
